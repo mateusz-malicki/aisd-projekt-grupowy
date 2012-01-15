151e55300b0e74319c286240e4818da77dabca60
Mateusz Malicki <mateusz.malicki1991@gmail.com>
2012-01-15 13:28:54 +0100

    add mutate() and initialize()

--- lib/breeding/generation.rb
+++ lib/breeding/generation.rb
@@ -1,4 +1,27 @@
 module Breeding
   class Generation
+    
+    def initialize(buses = nil)
+      @@coefficient = 0.05;
+      @@size = 10000;
+
+      if buses.nil?
+        @buses = Array.new(size)
+        @buses.each do |bus|
+          bus = Bus.new
+        end
+      else
+        @buses = buses
+      end
+    end
+
+    def mutate
+      @buses.each do |bus|
+        if rand(10000)/10000 <= @@coefficient
+          bus.mutate
+        end
+      end
+    end
+
   end
 end
